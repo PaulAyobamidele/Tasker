afed120c9e7e29ce01ab3c91be16a17bc3ec710a
Ayobamidele <paulikeystreble@gmail.com>
2023-02-07 21:39:19 +0100

    Task Tracker

---
+++ taskapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .serializers import TaskSerializer
+from rest_framework import viewsets
+from .models import Task
+
+
+
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
